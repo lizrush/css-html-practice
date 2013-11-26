7cab7f7956e2db71d1726051c92602f27a6e113c
liz rush <liz@lizmrush.com>
2013-11-26 15:02:00 -0800

    fixed bd create script

--- create_script.rb
+++ create_script.rb
@@ -3,6 +3,17 @@ require 'sequel'
 db = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://happyhour.db')
 db.create_table(:happyhour) do
   primary_key :id
-  string :location
-  integer :votes, default: 0
+  String :location
+  Integer :votes, default: 0
+  # add column for date created, then use ruby magic to determine current weeks bars and only display those ones
 end
+
+
+
+# to delete
+
+# heroku pg:reset DATABASE_URL --confirm liz-sinatra
+
+# to create_script
+
+# heroku run ruby create_script.rb
