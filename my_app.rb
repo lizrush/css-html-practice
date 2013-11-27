d87c8db01eea6ad0cb8f61ea174ed5451c88c093
liz rush <liz@lizmrush.com>
2013-11-27 15:20:32 -0800

    added yaml and date metadata to blog posts

--- my_app.rb
+++ my_app.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sequel'
-require 'sinatra'
+require 'yaml'
+require 'sinatra/base'
 
 
 class MyApp < Sinatra::Base
@@ -8,6 +9,10 @@ class MyApp < Sinatra::Base
 before do
   @db = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://happyhour.db')
   @happyhour = @db[:happyhour]
+
+  @posts = Dir.glob("views/posts/*.erb").map do |post_name|
+      post_name.split("/").last.slice(0..-5)
+  end
 end
 
   get "/" do
@@ -29,12 +34,29 @@ end
     erb :cute_pictures_of_animals, :locals => {:title => "~*pics of animals*~"} 
   end
 
-  get "/blog/:post_name" do
-    params[:post_name]
-    erb "/posts/#{params[:post_name]}".to_sym
+  get "/blog" do
+    erb :blog, :locals => {:title => "blog"}
   end
 
+  get "/blog/:post_name" do
+    page = erb("/posts/#{params[:post_name]}".to_sym, layout: false)
+    page_body = page.split("\n\n", 2).last
+    erb page_body, :locals => {:title => "blog"}
+  end
 
+  def meta_data
+    if @meta_data
+      @meta_data
+    else
+      @meta_data = {}
+      @posts.each do |posts|
+      html = erb("/posts/#{post}".to_sym, layout: false)
+      meta = YAML.load(html.split("\n\n", 2).first)
+      @meta_data[post] = meta
+      end
+    end
+    @meta_data
+   end
 end
 
 
