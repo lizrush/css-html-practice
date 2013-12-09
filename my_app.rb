b0e38bf47bf1cffa767a87347df32570cd79c85c
liz rush <liz@lizmrush.com>
2013-12-08 23:33:30 -0800

    alt styling practice for blog

--- my_app.rb
+++ my_app.rb
@@ -9,11 +9,11 @@ class MyApp < Sinatra::Base
 before do
   @db = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://happyhour.db')
   @happyhour = @db[:happyhour]
-
   @posts = Dir.glob("views/posts/*.erb").map do |post_name|
-      post_name.split("/").last.slice(0..-5)
+      post_name.split("/").last.gsub(".erb", "")
+    end
+  @sorted_posts = meta_data.sort_by {|post, date_hash| date_hash["date"]}.reverse.map{|post, date_hash| post}
   end
-end
 
   get "/" do
     erb :index, :locals => {:title => "~*ada happy hour voting mechanism*~"} 
@@ -35,13 +35,17 @@ end
   end
 
   get "/blog" do
-    erb :blog, :locals => {:title => "~*blog*~"}
+    @post_content = ""
+    @sorted_posts.each { |post|
+      page = erb "/posts/#{post}".to_sym, layout: false
+      @post_content += page.split("\n\n", 2).last }
+    erb :blog, :layout => :bloglayout, :locals => {:title => "liz's ada blog"}
   end
 
   get "/blog/:post_name" do
     page = erb("/posts/#{params[:post_name]}".to_sym, layout: false)
     page_body = page.split("\n\n", 2).last
-    erb page_body, :locals => {:title => "~*blog*~"}
+    erb page_body, :layout => :bloglayout
   end
 
   def meta_data
@@ -50,9 +54,9 @@ end
     else
       @meta_data = {}
       @posts.each do |post|
-      html = erb("/posts/#{post}".to_sym, layout: false)
-      meta = YAML.load(html.split("\n\n", 2).first)
-      @meta_data[post] = meta
+        html = erb("/posts/#{post}".to_sym, layout: false)
+        meta = YAML.load(html.split("\n\n", 2).first)
+        @meta_data[post] = meta
       end
     end
     @meta_data
